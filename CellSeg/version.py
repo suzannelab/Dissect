f07f384014c60a0f20bb6e7b535bc89845bc33e9
Sophie THEIS <sophiets13@gmail.com>
2020-03-16 12:28:14 +0100

    Add clean_skeleton function

--- CellSeg/version.py
+++ CellSeg/version.py
@@ -3,8 +3,8 @@
 #
 short_version = '0.0.1'
 version = '0.0.1'
-full_version = '0.0.1.dev0+3fda96d'
-git_revision = '3fda96d043dc8ff9015a0f25ae74a0094145ec69'
+full_version = '0.0.1.dev0+d228a7b'
+git_revision = 'd228a7b224dcce63c7ad6de2c0ea10be701f7372'
 release = False
 if not release:
     version = full_version
