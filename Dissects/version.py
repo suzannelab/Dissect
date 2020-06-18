3e3ad7c6633227a491ece8b4d8051da7a420096f
Sophie THEIS <sophiets13@gmail.com>
2020-06-18 17:14:56 +0200

    Remove and stop track __pycache__

--- Dissects/version.py
+++ Dissects/version.py
@@ -3,8 +3,8 @@
 #
 short_version = '0.0.1'
 version = '0.0.1'
-full_version = '0.0.1.dev0+e894cc4'
-git_revision = 'e894cc431f085e67f3f40028459d374887ffc795'
+full_version = '0.0.1.dev0+28fdaf2'
+git_revision = '28fdaf21863e643882e29babe2a4388f2e30cc9f'
 release = False
 if not release:
     version = full_version
