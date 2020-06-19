a11ad27b25634c38926c094226e649ed55bf49ce
Sophie THEIS <sophiets13@gmail.com>
2020-06-19 16:52:29 +0200

    Add some file

--- Dissects/version.py
+++ Dissects/version.py
@@ -3,8 +3,8 @@
 #
 short_version = '0.0.1'
 version = '0.0.1'
-full_version = '0.0.1.dev0+6ec80d4'
-git_revision = '6ec80d43e2573a1b22cc82f2c9f09629a2f07a8f'
+full_version = '0.0.1.dev0+2f47627'
+git_revision = '2f47627a0e1666eebb883088f58f19ca4d13266c'
 release = False
 if not release:
     version = full_version
