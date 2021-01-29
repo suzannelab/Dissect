69ee4a13804768ad6be71d0ddc3ab0ccd3b559ca
TatiaM <tatiana.merle@lilo.org>
2021-01-29 16:54:24 +0100

    Segmentation function 02 with a maximum (maximum number of pixels of a cell). Still need to be improved to suggest a min and max at one or two sigmas after the 1rst segmentation process -> thus needs to be done in two steps.

--- Dissects/segmentation/seg_2D.py
+++ Dissects/segmentation/seg_2D.py
@@ -36,6 +36,38 @@ def segmentation(mask, min_area=None):
     return segmentation
 
 
+def segmentation2(mask, min_area=None, max_area=None):
+    """
+    Segment the cells of the image.
+
+    Paramaters
+    ----------
+    mask: np.array, filament=1 and background=0
+    min_area: integer, minimum number of pixels of a cell. Under this value, a cell is 0
+    max_area: interger, maximum number of pixels of a cell. Above this value, a cell is 1
+    Return
+    ------
+    segmentation: np.array
+        Pixels of filaments are equal to 0
+        Pixels of the background = 1
+        Pixels of cell i = i
+    """
+    edges = filters.sobel(mask)
+    markers = np.zeros_like(mask)
+    markers[mask == 0] = 2
+    markers[mask > 0] = 1
+
+    segmentation = ski_seg.watershed(edges, markers)
+    segmentation, _ = ndi.label(segmentation == 2)
+    if min_area is not None:
+        segmentation = morphology.remove_small_objects(segmentation, min_area)
+
+    if max_area is not None:
+        seg_max = morphology.remove_small_objects(segmentation, max_area)
+        segmentation[np.where(seg_max!=0)] = 1
+    return segmentation
+
+
 def junction_around_cell(mask, seg, cell):
     """Find junctions around cell i.
 
