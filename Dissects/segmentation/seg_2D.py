4cf3fd670c5bf8288aa6989f3deb8cfdfaaaa5d3
TatiaM <tatiana.merle@lilo.org>
2021-02-03 10:17:35 +0100

    Vertices function. Segmentation function imposed with min_area=0 and working on thinmask.

--- Dissects/segmentation/seg_2D.py
+++ Dissects/segmentation/seg_2D.py
@@ -82,7 +82,9 @@ def junction_around_cell(mask, seg, cell):
     return juncelli
 
 
-def vertices(mask, seg=None, dilation_width=1):
+
+
+def vertices(mask, max_area):
     """Find the vertices (nodes) of the skeleton
 
     Parameters
@@ -94,72 +96,76 @@ def vertices(mask, seg=None, dilation_width=1):
     image_vertex: np.array where cells=1, junctions=2, vertices >=3
     list_vertices: list of the vertices
     df_vertices: dataframe with for each vertex x_0: x coordinate of the vertex
-                                                y_0: y coordinate of the vertex
-                                                Cell_i: the cells connected to the vertex
-
+						y_0: y coordinate of the vertex
+						Cell_i: the cells connected to the vertex
+    seg0: numpy.array
+    segmented image with no minimal size cell exclusion
 
     """
 
-    if seg is None:
-        seg = segmentation(mask)
-
+    thinmask = thinning(mask, 2)
+    seg0 = segmentation(thinmask, auto_remove=False, min_area = 0, max_area=max_area)
     image_vertex = np.zeros_like(mask)
 
-    for i in range(1, np.unique(seg)[-1] + 1):
+    for i in range(1, np.unique(seg0)[-1]+1):
         image_cell_mask_i = np.zeros_like(mask)
-        image_cell_mask_i[np.where(seg == i)] = 1
-        seg_dilate_i = dilation(image_cell_mask_i, dilation_width)
+        image_cell_mask_i[np.where(seg0 == i)] = 1
+        seg_dilate_i = dilation(image_cell_mask_i, 1)
         image_vertex = image_vertex + seg_dilate_i
-    list_vertices = np.where(image_vertex >= 3)  # Récupération des vextex 'simples'
+    list_vertices = np.where(image_vertex >= 3) #Récupération des vextex 'simples'
 
     columns_name = ['x_0',
-                    'y_0',
-                    'Cell_1',
-                    'Cell_2',
-                    'Cell_3',
-                    'Cell_4',
-                    'Cell_5']
+                'y_0',
+                'Cell_1',
+                'Cell_2',
+                'Cell_3',
+                'Cell_4',
+                'Cell_5']
     nb_vertices = len(list_vertices[0])
-    init = np.zeros((nb_vertices, len(columns_name)))
+    init = np.zeros((nb_vertices , len(columns_name)))
 
     df_vertices = pd.DataFrame(data=init, columns=columns_name)
 
     for v in range(0, len(list_vertices[0])):
-        df_vertices.loc[v]['x_0'] = list_vertices[0][v]
-        df_vertices.loc[v]['y_0'] = list_vertices[1][v]
 
-        carre = seg[list_vertices[0][v] - 3: list_vertices[0][v] +
-                    4, list_vertices[1][v] - 3: list_vertices[1][v] + 4]
+        carre = seg0[max(0,list_vertices[0][v]-3) : min(list_vertices[0][v]+4,seg0.shape[0]-1), 
+                     max(0,list_vertices[1][v]-3) :  min(list_vertices[1][v]+4,seg0.shape[1]-1)]
         cells = np.unique(carre)
+        print(cells)
+        if len(cells)>0:
+            
+            df_vertices.loc[v]['x_0'] = list_vertices[0][v]
+            df_vertices.loc[v]['y_0'] = list_vertices[1][v]
+            df_vertices.loc[v]['Cell_1'] = cells[1]
+            df_vertices.loc[v]['Cell_2'] = cells[2]
+            df_vertices.loc[v]['Cell_3'] = cells[3]
 
-        df_vertices.loc[v]['Cell_1'] = cells[1]
-        df_vertices.loc[v]['Cell_2'] = cells[2]
-        df_vertices.loc[v]['Cell_3'] = cells[3]
-
-        if len(np.unique(carre)) == 4:
-            df_vertices.loc[v]['Cell_4'] = 'Nan'
-            df_vertices.loc[v]['Cell_5'] = 'Nan'
-        if len(np.unique(carre)) == 5:
-            df_vertices.loc[v]['Cell_4'] = cells[4]
-            df_vertices.loc[v]['Cell_5'] = 'Nan'
-        if len(np.unique(carre)) == 6:
-            df_vertices.loc[v]['Cell_4'] = cells[4]
-            df_vertices.loc[v]['Cell_5'] = cells[5]
+            if len(np.unique(carre)) == 4 :
+                df_vertices.loc[v]['Cell_4'] = 'Nan'
+                df_vertices.loc[v]['Cell_5'] = 'Nan'
+            if len(np.unique(carre)) == 5 :
+                df_vertices.loc[v]['Cell_4'] = cells[4]
+                df_vertices.loc[v]['Cell_5'] = 'Nan'
+            if len(np.unique(carre)) == 6 :
+                df_vertices.loc[v]['Cell_4'] = cells[4]
+                df_vertices.loc[v]['Cell_5'] = cells[5]
 
     ind = 0
-    while ind < len(df_vertices):
+    while ind < len(df_vertices) :
+        #print(df_vertices.shape[0])
         cells_ind = np.array([df_vertices['Cell_1'].iloc[ind],
-                              df_vertices['Cell_2'].iloc[ind],
-                              df_vertices['Cell_3'].iloc[ind],
-                              df_vertices['Cell_4'].iloc[ind],
-                              df_vertices['Cell_5'].iloc[ind]])
+                                df_vertices['Cell_2'].iloc[ind],
+                                df_vertices['Cell_3'].iloc[ind],
+                                df_vertices['Cell_4'].iloc[ind],
+                                df_vertices['Cell_5'].iloc[ind]])
 
         liste_x0 = [df_vertices['x_0'].iloc[ind]]
         liste_y0 = [df_vertices['y_0'].iloc[ind]]
         liste_i = []
         liste_cellsi = [cells_ind]
 
-        for i in range(ind + 1, len(df_vertices)):
+
+        for i in range(ind+1, len(df_vertices)):
 
             cells_i = np.array([df_vertices['Cell_1'].iloc[i],
                                 df_vertices['Cell_2'].iloc[i],
@@ -167,35 +173,42 @@ def vertices(mask, seg=None, dilation_width=1):
                                 df_vertices['Cell_4'].iloc[i],
                                 df_vertices['Cell_5'].iloc[i]])
 
-            mask_TrueFalse = np.isin(cells_ind, cells_i)
 
-            if np.sum(mask_TrueFalse) >= 3:
+            mask_TrueFalse = np.isin(cells_ind, cells_i)
+            
+            
+            
+
+            if np.sum(mask_TrueFalse) > 3 and np.isin(1., cells_i) and np.isin(1., cells_ind):
                 liste_cellsi.append(cells_i)
                 liste_i.append(df_vertices.axes[0][i])
                 liste_x0.append(df_vertices['x_0'].iloc[i])
                 liste_y0.append(df_vertices['y_0'].iloc[i])
 
-        df_vertices['x_0'].iloc[ind] = int(np.round(np.mean(liste_x0)))
-        df_vertices['y_0'].iloc[ind] = int(np.round(np.mean(liste_y0)))
+            if np.sum(mask_TrueFalse) >= 3 and np.isin(1., cells_i)==False and np.isin(1., cells_ind)==False:
+                liste_cellsi.append(cells_i)
+                liste_i.append(df_vertices.axes[0][i])
+                liste_x0.append(df_vertices['x_0'].iloc[i])
+                liste_y0.append(df_vertices['y_0'].iloc[i])
+        
+        df_vertices['x_0'].iloc[ind]=int(np.round(np.mean(liste_x0)))
+        df_vertices['y_0'].iloc[ind]=int(np.round(np.mean(liste_y0)))
 
         ucells = np.unique(liste_cellsi)
 
-        try:
-            df_vertices['Cell_1'].iloc[ind] = ucells[0]
-            df_vertices['Cell_2'].iloc[ind] = ucells[1]
-            df_vertices['Cell_3'].iloc[ind] = ucells[2]
-            df_vertices['Cell_4'].iloc[ind] = ucells[3]
-            df_vertices['Cell_5'].iloc[ind] = ucells[4]
-        except:
-            pass
+        df_vertices['Cell_1'].iloc[ind] = ucells[0]
+        df_vertices['Cell_2'].iloc[ind] = ucells[1]
+        df_vertices['Cell_3'].iloc[ind] = ucells[2]
+        df_vertices['Cell_4'].iloc[ind] = ucells[3]
+        df_vertices['Cell_5'].iloc[ind] = ucells[4]
 
-        # print(liste_i)
-        df_vertices = df_vertices.drop(liste_i)
+        #print(liste_i)
+        df_vertices=df_vertices.drop(liste_i)
 
-        ind += 1
+        ind+=1
 
-    df_vertices.reset_index(drop=True, inplace=True)
-    return image_vertex, list_vertices, df_vertices
+    return image_vertex, list_vertices, df_vertices, seg0
+
 
 
 def junctions(list_vertices, df_vertices):
