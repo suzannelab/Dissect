74b385cfd3e1b2a27e051e602c201527199df607
Sophie <sophiets13@gmail.com>
2021-10-22 10:54:34 +0200

    Create segmentation class

---
+++ Dissects/segmentation/segmentation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as ipv_draw
+
+from ..geometry import Skeleton
+
+class Segmentation:
+
+    def __init__(self):
+        self.face_df = pd.DataFrame()
+        self.edge_df = pd.DataFrame()
+        self.vert_df = pd.DataFrame()
+        self.point_df = pd.DataFrame()
+        self.specs = dict()
+
+
+    def __init__(self, skeleton):
+        self.skeleton = skeleton
+        self.face_df = pd.DataFrame()
+        self.edge_df = pd.DataFrame()
+        self.vert_df = pd.DataFrame()
+        self.point_df = pd.DataFrame()
+        self.specs = dict()
+
+
+
